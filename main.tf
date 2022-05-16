7a5000de9b2b462ce62f05e7ea14ed514b7d08fe
Cristian Chaparro Cuadros <104446290+cchaparroart@users.noreply.github.com>
2022-05-16 15:45:08 -0500

    provider

--- main.tf
+++ main.tf
@@ -7,7 +7,11 @@ terraform {
       source = "hashicorp/random"
     }
   }
-
+  provider "aws" {
+    version = "~>3.0"
+    region  = "east-us-1"
+  }
+  
   cloud {
     organization = "cchaparro"
 
