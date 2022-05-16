5335c4862fd791d24a40baa6504eb8c65e850e3b
Cristian Chaparro Cuadros <104446290+cchaparroart@users.noreply.github.com>
2022-05-16 15:25:32 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,3 @@ variable "AWS_SECRET_ACCESS_KEY" {}
 variable "AWS_ACCESS_KEY_ID" {} 
 variable "aws_region" {}
 variable "app_prefix" {}
-variable "stage_name" {
-  default = "dev"
-  type    = "string"
-}
