318cb4295856f96e2fe3cc9cdff9f8593c8ef5e7
Cristian Chaparro Cuadros <104446290+cchaparroart@users.noreply.github.com>
2022-05-16 15:54:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,16 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      
     }
     random = {
       source = "hashicorp/random"
     }
   }
-  provider "aws" {
-    version = "~>3.0"
-    region  = "east-us-1"
-  }
-  
+
   cloud {
     organization = "cchaparro"
 
@@ -19,4 +16,8 @@ terraform {
       name = "gh-demo-shopping"
     }
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
