d4d98c74d509635746d7c8ad6a84e11537e43010
Cristian Chaparro Cuadros <104446290+cchaparroart@users.noreply.github.com>
2022-05-16 15:31:55 -0500

    borrando

--- provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  version    = "4.14.0"
-  region     = var.aws_region
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
--- variables.tf
+++
@@ -1,3 +0,0 @@
-variable "AWS_SECRET_ACCESS_KEY" {} 
-variable "AWS_ACCESS_KEY_ID" {} 
-variable "aws_region" {}
